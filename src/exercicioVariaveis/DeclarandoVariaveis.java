7cad253a622bb6127a0f2beb64854c60b055a945
JorgeFVSz <jorge.silvestre@zup.com.br>
2024-12-02 16:40:54 -0300

    Exercício Variáveis

---
+++ src/exercicioVariaveis/DeclarandoVariaveis.java
@@ -0,0 +1,14 @@
+package exercicioVariaveis;
+
+public class DeclarandoVariaveis {
+
+    public static void main(String[] args) {
+
+        String nome = "Jorge";
+        int idade = 25;
+        double altura = 1.67;
+        boolean isMatriculado = true;
+
+        System.out.println("Nome: " + nome + " Idade: " + idade + " Altura: " + altura + " Você está matriculado no curso? " + isMatriculado);
+    }
+}
